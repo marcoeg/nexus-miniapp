24188a06ba447597d8886f102221f4aba6eb6b59
Marco Graziano <marco@graziano.com>
2025-05-01 16:39:06 -0700

    initial setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
+  base: '/nexus-miniapp/',
   css: {
     preprocessorOptions: {
       scss: {
